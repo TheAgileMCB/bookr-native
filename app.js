19d7320f0ea26e377907927d46efc8a53bab5f7e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-30 17:25:16 -0500

    couldn't access map component, so moved maps to App

--- app.js
+++ app.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import MapView from 'react-native-maps'
+
+export default class Map extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 41.977879,
+            longitude: -91.665627,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+
+          <MapView.Marker
+            coordinate={{
+              latitude: 41.977879,
+              longitude: -91.665627,
+            }}>
+          </MapView.Marker>
+        </MapView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
   },
-});
+  map: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute'
+  },
+})
