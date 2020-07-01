75bbe2cac68ff0fb5f61c93531a4d53423351520
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-30 19:49:43 -0500

    refactored app file to pass state

--- app.js
+++ app.js
@@ -1,84 +1,31 @@
-import 'react-native-gesture-handler';
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  Button,
-  Dimensions,
-  View,
-} from 'react-native';
+import React from 'react';
+import AppNavigator from './AppNavigator';
 
-// import MapView from 'react-native-maps';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-// const {width, height} = Dimensions.get('window')
-
-// const SCREEN_HEIGHT = height
-// const SCREEN_WIDTH = width
-// const ASPECT_RATIO = width / height
-// const LATITUDE_DELTA = 0.0922
-// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-          <Button
-            title="Locate Me!"
-            onPress={() =>
-              navigation.navigate('Map', { name: 'map' })
-            }
-          />
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-export default function App(){
-
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      }
+    }
+  }
+  render() {
     return (
-      <NavigationContainer>
-       <Stack.Navigator>
-         <Stack.Screen name="Home" component={HomeScreen} />
-       </Stack.Navigator>
-      </NavigationContainer>
+      <AppNavigator
+        screenProps={{
+          initialPosition: this.state.initialPosition,
+          markerPosition: this.state.markerPosition,
+        }}
+      />
     );
+  }
 }
-
-const styles = StyleSheet.create({
-  radius: {
-    height: 50,
-    width: 50,
-    borderRadius: 50 / 2,
-    overflow: 'hidden',
-    backgroundColor: 'rgba(0, 122, 255, 0.1)',
-    borderWidth: 1,
-    borderColor: 'rgba(0, 112, 255, 0.3)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  marker: {
-    height: 20,
-    width: 20,
-    borderWidth: 3,
-    borderColor: 'white',
-    borderRadius: 20 / 2,
-    overflow: 'hidden',
-    backgroundColor: '#007aff',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  map: {
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    position: 'absolute'
-  },
-})
