49555ee3e0e1874842fa296f9f5671ab514cd80e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-30 19:05:42 -0500

    updated App as home screen and with nav

--- app.js
+++ app.js
@@ -1,98 +1,50 @@
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   Dimensions,
   View,
 } from 'react-native';
 
-import MapView from 'react-native-maps'
+// import MapView from 'react-native-maps';
 
-const {width, height} = Dimensions.get('window')
+// const {width, height} = Dimensions.get('window')
 
-const SCREEN_HEIGHT = height
-const SCREEN_WIDTH = width
-const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.0922
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
+// const SCREEN_HEIGHT = height
+// const SCREEN_WIDTH = width
+// const ASPECT_RATIO = width / height
+// const LATITUDE_DELTA = 0.0922
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
-export default class Map extends Component {
-  constructor(props) {
-    super(props)
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+          <Button
+            title="Locate Me!"
+            onPress={() =>
+              navigation.navigate('Map', { name: 'map' })
+            }
+          />
+    </View>
+  );
+}
 
-    this.state = {
-      initialPosition: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0,
-        longitudeDelta: 0,
-      },
-      markerPosition: {
-        latitude: 0,
-        longitude: 0,
-      }
-    }
-  }
+const Stack = createStackNavigator();
 
-  watchID: ?number = null
+export default function App(){
 
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var initialRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      }
-
-      this.setState({initialPosition: initialRegion})
-      this.setState({markerPosition: initialRegion})
-    },
-    (error) => alert(JSON.stringify(error)),
-    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
-
-    this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lat = parseFloat(position.coords.latitude)
-      var long = parseFloat(position.coords.longitude)
-
-      var lastRegion = {
-        latitude: lat,
-        longitude: long,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      }
-
-      this.setState({initialPosition: lastRegion})
-      this.setState({markerPosition: lastRegion})
-    })
-  }
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID)
-  }
-
-  render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          region={this.state.initialPosition}>
-
-          <MapView.Marker
-            coordinate={this.state.markerPosition}>
-            <View style={styles.radius}>
-              <View style={styles.marker}>
-              </View>
-            </View>
-          </MapView.Marker>
-        </MapView>
-      </View>
+      <NavigationContainer>
+       <Stack.Navigator>
+         <Stack.Screen name="Home" component={HomeScreen} />
+       </Stack.Navigator>
+      </NavigationContainer>
     );
-  }
 }
 
 const styles = StyleSheet.create({
